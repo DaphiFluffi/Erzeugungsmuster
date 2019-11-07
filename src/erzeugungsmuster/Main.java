acbc893fc0d45485f12dcfc49c5d1f5270525619
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2019-11-07 15:51:10 +0100

    e2 und p2 in Main erzeugt und eingebunden

--- src/erzeugungsmuster/Main.java
+++ src/erzeugungsmuster/Main.java
@@ -5,15 +5,18 @@ public class Main {
 	public static void main(String args[]) {
 		
 		Erzeuger e1;
+		Erzeuger e2;
+
 		Produkt p1;
+		Produkt p2;
 		
 		e1 = new KonkreterErzeugerA();
 		p1 = e1.fabrikmethode();
 		p1.print();
 		
-		e1 = new KonkreterErzeugerB();
-		p1 = e1.fabrikmethode();
-		p1.print();
+		e2 = new KonkreterErzeugerB();
+		p2 = e2.fabrikmethode();
+		p2.print();
 		
 		
 	}
