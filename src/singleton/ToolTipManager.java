3f0e8a6bb57a02bcdc78ae4df59bbf45232f3ab7
HuePhung <56964065+HuePhung@users.noreply.github.com>
2019-11-21 14:37:12 +0100

    TTP1

--- src/singleton/ToolTipManager.java
+++ src/singleton/ToolTipManager.java
@@ -1,5 +1,26 @@
 package singleton;
 
-public class ToolTipManager {
+final public class ToolTipManager {
+	
+	private static ToolTipManager instance;
+	
+	private ToolTipManager() {
+		System.out.println("Ein neues Singleton erzeugt");
+	
+	}
+	
+	
+	public static ToolTipManager getInstance() {
+		
+		if ( instance == null) {
+			instance = new ToolTipManager();
+		}
+		return instance;
+	}
+	
+	public void operation() {
+		System.out.println("operation() aufgerufen");
+	}
+	
 
 }
